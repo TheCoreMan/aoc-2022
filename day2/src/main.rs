a896a0052b405e6b4af93805a96a73cf266e8785
Shay Nehmad <shayn@recolabs.ai>
2022-12-03 21:45:34 +0200

    day2: implemented part 2, got a ⭐️

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -15,12 +15,16 @@ fn new_shape(encoded_shape: &str) -> Shape {
     }
 }
 
-fn calc_round_score(opponent_shape: Shape, my_shape: Shape) -> i32 {
-    let shape_score = match my_shape {
+fn get_shape_score(s: &Shape) -> i32 {
+    return match s {
         Shape::Rock => 1,
         Shape::Paper => 2,
         Shape::Scissors => 3,
     };
+}
+
+fn calc_round_score(opponent_shape: Shape, my_shape: Shape) -> i32 {
+    let shape_score = get_shape_score(&my_shape);
 
     // 0 for loss
     // 3 for draw
@@ -46,6 +50,55 @@ fn calc_round_score(opponent_shape: Shape, my_shape: Shape) -> i32 {
     return shape_score + outcome_score;
 }
 
+enum Outcome {
+    Win,
+    Draw,
+    Lose,
+}
+
+fn new_outcome(encoded_outcome: &str) -> Outcome {
+    match encoded_outcome {
+        "Z" => Outcome::Win,
+        "Y" => Outcome::Draw,
+        "X" => Outcome::Lose,
+        _ => panic!("Unknown outcome encoding {}", encoded_outcome),
+    }
+}
+
+// It was at this point that I realized that there's probably a nice data
+// structure to represent the rules of Rock Paper Scissors, but that in the end
+// we're all going to die so match on enums in Good Enough ™️.
+
+fn calc_round_score_v2(opponent_shape: Shape, needed_outcome: Outcome) -> i32 {
+    let outcome_score = match needed_outcome {
+        Outcome::Win => 6,
+        Outcome::Draw => 3,
+        Outcome::Lose => 0,
+    };
+
+    let my_shape = match needed_outcome {
+        Outcome::Win => match opponent_shape {
+            Shape::Rock => Shape::Paper,
+            Shape::Paper => Shape::Scissors,
+            Shape::Scissors => Shape::Rock,
+        },
+        Outcome::Draw => match opponent_shape {
+            Shape::Rock => Shape::Rock,
+            Shape::Paper => Shape::Paper,
+            Shape::Scissors => Shape::Scissors,
+        },
+        Outcome::Lose => match opponent_shape {
+            Shape::Rock => Shape::Scissors,
+            Shape::Paper => Shape::Rock,
+            Shape::Scissors => Shape::Paper,
+        },
+    };
+
+    let shape_score = get_shape_score(&my_shape);
+
+    return shape_score + outcome_score;
+}
+
 fn main() {
     println!("Advent of Code 🎄🎄 2022 // Day 2");
 
@@ -57,9 +110,21 @@ fn main() {
     let mut score_total = 0;
     for round in rounds {
         let round_shapes: Vec<&str> = round.split(" ").collect();
-        let my_shape: Shape = new_shape(round_shapes[1]);
         let opponent_shape: Shape = new_shape(round_shapes[0]);
+        let my_shape: Shape = new_shape(round_shapes[1]);
         score_total += calc_round_score(opponent_shape, my_shape);
+    }
+
+    println!("Score total: {}", score_total);
+
+    let rounds_again = strategy_guide.split("\n");
+
+    let mut score_total = 0;
+    for round in rounds_again {
+        let round_shapes: Vec<&str> = round.split(" ").collect();
+        let opponent_shape: Shape = new_shape(round_shapes[0]);
+        let needed_outcome: Outcome = new_outcome(round_shapes[1]);
+        score_total += calc_round_score_v2(opponent_shape, needed_outcome);
     }
 
     println!("Score total: {}", score_total)
